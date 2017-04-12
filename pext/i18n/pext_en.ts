6abff23bf5edc4e10af87b1c2e61c7bb39c5b50b
Sylvia van Os <sylvia@hackerchick.me>
2017-04-12 21:42:59 +0200

    Add English

---
+++ pext/i18n/pext_en.ts
@@ -0,0 +1,235 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../qml/AboutDialog.qml" line="24"/>
+        <source>About Pext</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InstallModuleFromRepositoryDialog</name>
+    <message>
+        <location filename="../qml/InstallModuleFromRepositoryDialog.qml" line="35"/>
+        <source>Where do you want to get modules from?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InstallModuleFromRepositoryNoModulesAvailableDialog</name>
+    <message>
+        <location filename="../qml/InstallModuleFromRepositoryNoModulesAvailableDialog.qml" line="27"/>
+        <source>No modules found in this repository.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InstallModuleFromRepositorySelectModuleDialog</name>
+    <message>
+        <location filename="../qml/InstallModuleFromRepositorySelectModuleDialog.qml" line="34"/>
+        <source>Choose the module to install:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/InstallModuleFromRepositorySelectModuleDialog.qml" line="44"/>
+        <source>Choose the preferred download source:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/InstallModuleFromRepositorySelectModuleDialog.qml" line="54"/>
+        <source>Module information:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/InstallModuleFromRepositorySelectModuleDialog.qml" line="58"/>
+        <source>Developer: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/InstallModuleFromRepositorySelectModuleDialog.qml" line="62"/>
+        <source>Description: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/InstallModuleFromRepositorySelectModuleDialog.qml" line="66"/>
+        <source>License: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/InstallModuleFromRepositorySelectModuleDialog.qml" line="70"/>
+        <source>As Pext modules are code, please make sure you trust the developer before continuing.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InstallModuleFromURLDialog</name>
+    <message>
+        <location filename="../qml/InstallModuleFromURLDialog.qml" line="33"/>
+        <source>Enter the git URL of the module to install:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/InstallModuleFromURLDialog.qml" line="42"/>
+        <source>As Pext modules are code, please make sure you trust the developer before continuing.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ListModulesDialog</name>
+    <message>
+        <location filename="../qml/ListModulesDialog.qml" line="33"/>
+        <source>Installed modules:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LoadModuleDialog</name>
+    <message>
+        <location filename="../qml/LoadModuleDialog.qml" line="34"/>
+        <source>Choose the module to load:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/LoadModuleDialog.qml" line="44"/>
+        <source>Enter module settings (leave blank for defaults):</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NoModulesInstalledDialog</name>
+    <message>
+        <location filename="../qml/NoModulesInstalledDialog.qml" line="27"/>
+        <source>No modules installed, please install one first.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UninstallModuleDialog</name>
+    <message>
+        <location filename="../qml/UninstallModuleDialog.qml" line="34"/>
+        <source>Choose the module to uninstall:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UpdateModuleDialog</name>
+    <message>
+        <location filename="../qml/UpdateModuleDialog.qml" line="34"/>
+        <source>Choose the module to update:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="194"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="200"/>
+        <source>Quit without saving</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="209"/>
+        <source>Reload active module</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="216"/>
+        <source>Close active module</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="228"/>
+        <source>Load module</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="246"/>
+        <source>List installed modules</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="258"/>
+        <source>Install module</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="263"/>
+        <source>From online module list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="284"/>
+        <source>From URL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="296"/>
+        <source>Uninstall module</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="315"/>
+        <source>Update module</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="334"/>
+        <source>Update all modules</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="349"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="358"/>
+        <source>Visit homepage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="378"/>
+        <source>Clear</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="378"/>
+        <source>Back</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="387"/>
+        <source>Type to search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="430"/>
+        <source>Welcome to Pext</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="431"/>
+        <source>To get started, press &lt;kbd&gt;%1&lt;/kbd&gt; to open a new tab.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="432"/>
+        <source>When you are done with a tab, you can always close it by pressing &lt;kbd&gt;%1&lt;/kbd&gt;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../qml/main.qml" line="433"/>
+        <source>You currently have %n module(s) installed. You can manage modules in the settings menu.</source>
+        <translation type="unfinished">
+            <numerusform>You currently have %n module installed. You can manage modules in the settings menu.</numerusform>
+            <numerusform>You currently have %n modules installed. You can manage modules in the settings menu.</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
