75b89199a9251aba9663c06aacc9fcb9206d6801
Heimen Stoffels <vistausss@outlook.com>
2017-06-29 16:43:06 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (78 of 78 strings)
    
    Translation: Pext/Translations
    Translate-URL: https://hosted.weblate.org/projects/pext/translations/nl/

--- pext/i18n/pext_nl.ts
+++ pext/i18n/pext_nl.ts
@@ -11,32 +11,32 @@
     <message>
         <location filename="../qml/AboutDialog.qml" line="38"/>
         <source>Copyright</source>
-        <translation type="unfinished"></translation>
+        <translation>Copyright</translation>
     </message>
     <message>
         <location filename="../qml/AboutDialog.qml" line="57"/>
         <source>Translators</source>
-        <translation type="unfinished"></translation>
+        <translation>Vertalers</translation>
     </message>
     <message>
         <location filename="../qml/AboutDialog.qml" line="64"/>
         <source>Want to help translate Pext? Please click here.</source>
-        <translation type="unfinished"></translation>
+        <translation>Wilt u meehelpen met het vertalen van Pext? Klik dan hier.</translation>
     </message>
     <message>
         <location filename="../qml/AboutDialog.qml" line="65"/>
         <source>The Pext team would like to thank the following users for translating Pext:</source>
-        <translation type="unfinished"></translation>
+        <translation>Het Pext-team bedankt de volgende gebruikers voor het vertalen van Pext:</translation>
     </message>
     <message>
         <location filename="../qml/AboutDialog.qml" line="66"/>
         <source>Chinese (Traditional)</source>
-        <translation type="unfinished"></translation>
+        <translation>Chinees (Traditioneel)</translation>
     </message>
     <message>
         <location filename="../qml/AboutDialog.qml" line="68"/>
         <source>Dutch</source>
-        <translation type="unfinished"></translation>
+        <translation>Nederlands</translation>
     </message>
 </context>
 <context>
@@ -126,7 +126,7 @@
     <message>
         <location filename="../qml/InstallThemeFromRepositoryDialog.qml" line="35"/>
         <source>Where do you want to get themes from?</source>
-        <translation>Waar wilt u thema&apos;s vandaan halen?</translation>
+        <translation>Waar wilt u thema's vandaan halen?</translation>
     </message>
 </context>
 <context>
@@ -134,7 +134,7 @@
     <message>
         <location filename="../qml/InstallThemeFromRepositoryNoThemesAvailableDialog.qml" line="27"/>
         <source>No themes found in this repository.</source>
-        <translation>Geen thema&apos;s gevonden in deze bron.</translation>
+        <translation>Geen thema's gevonden in deze bron.</translation>
     </message>
 </context>
 <context>
@@ -281,7 +281,7 @@
     <message>
         <location filename="../qml/NoThemesInstalledDialog.qml" line="27"/>
         <source>No themes installed, please install one first.</source>
-        <translation>Geen thema&apos;s geïnstalleerd; installeer er een.</translation>
+        <translation>Geen thema's geïnstalleerd; installeer er een.</translation>
     </message>
 </context>
 <context>
@@ -345,7 +345,7 @@
     <message>
         <location filename="../qml/main.qml" line="347"/>
         <source>Manage themes</source>
-        <translation>Thema&apos;s beheren</translation>
+        <translation>Thema's beheren</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="369"/>
@@ -360,7 +360,7 @@
     <message>
         <location filename="../qml/main.qml" line="407"/>
         <source>Update all themes</source>
-        <translation>Alle thema&apos;s bijwerken</translation>
+        <translation>Alle thema's bijwerken</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="418"/>
