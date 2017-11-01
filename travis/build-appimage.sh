28c8ada4059e99297e9acd809cc65fcdb4dc6f80
TheAssassin <theassassin@users.noreply.github.com>
2017-11-01 11:20:26 +0100

    Fix AppRun

--- travis/build-appimage.sh
+++ travis/build-appimage.sh
@@ -90,7 +90,7 @@ cat > AppDir/AppRun <<EAT
 #! /bin/sh
 
 # make sure to set APPDIR when run directly from the AppDir
-if [ -z \$APPDIR ]; then APPDIR=\$(readlink -f \$(dirname "$0")); fi
+if [ -z \$APPDIR ]; then APPDIR=\$(readlink -f \$(dirname "\$0")); fi
 
 export LD_LIBRARY_PATH="\$APPDIR"/usr/lib
 
