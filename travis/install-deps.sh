67046ea8ba6b0e3b72e7f3f0a7ed611fc8d17dee
TheAssassin <theassassin@users.noreply.github.com>
2017-11-09 03:31:28 +0100

    Add missing package

--- travis/install-deps.sh
+++ travis/install-deps.sh
@@ -4,7 +4,7 @@
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/TheAssassin:/AppImageLibraries/xUbuntu_14.04/ /' > /etc/apt/sources.list.d/curl-httponly.list"
 wget -nv https://download.opensuse.org/repositories/home:TheAssassin:AppImageLibraries/xUbuntu_14.04/Release.key -O- | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install -y curl-httponly
+sudo apt-get install -y curl-httponly libcurl3-httponly
 
 # install additional libraries
 sudo apt-get install -y libgit2
