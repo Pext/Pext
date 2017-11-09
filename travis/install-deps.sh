a783cb15f4691d3d4a55bf895e93d72abd46acd6
TheAssassin <theassassin@users.noreply.github.com>
2017-11-09 03:57:03 +0100

    Try to fix installation

--- travis/install-deps.sh
+++ travis/install-deps.sh
@@ -4,7 +4,7 @@
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/TheAssassin:/AppImageLibraries/xUbuntu_14.04/ /' > /etc/apt/sources.list.d/curl-httponly.list"
 wget -nv https://download.opensuse.org/repositories/home:TheAssassin:AppImageLibraries/xUbuntu_14.04/Release.key -O- | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install -y curl-httponly libcurl3-httponly libcurl4-gnutls-httponly-dev
+sudo apt-get install -y curl-httponly libcurl3-httponly libcurl4-gnutls-httponly-dev libcurl3-gnutls-httponly
 
 # install additional libraries
 sudo apt-get install -y libgit2-dev
