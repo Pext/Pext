1f70ba4b8ef5e568efc94689d359db3459640093
TheAssassin <theassassin@users.noreply.github.com>
2017-11-11 04:55:05 +0100

    Use static CMake

--- travis/install-deps.sh
+++ travis/install-deps.sh
@@ -5,10 +5,10 @@ sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/TheAssassi
 wget -nv https://download.opensuse.org/repositories/home:TheAssassin:AppImageLibraries/xUbuntu_14.04/Release.key -O- | sudo apt-key add -
 sudo apt-get update
 
-sudo apt-get purge -y curl libcurl\*
+sudo apt-get purge -y curl\* libcurl\*
+wget https://cmake.org/files/v3.10/cmake-3.10.0-rc3-Linux-x86_64.tar.gz -O- | sudo tar xz -C /usr --strip-components=1
 
 sudo apt-get install -y curl-httponly libcurl4-gnutls-httponly-dev libcurl3-gnutls-httponly libcurl3-httponly
-sudo apt-get install -y cmake
 
 # install additional libraries
 sudo apt-get install -y libgit2-dev
