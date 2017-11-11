59c6a83e8dd3618490bdfc2c2047f647184a8961
TheAssassin <theassassin@users.noreply.github.com>
2017-11-11 01:47:35 +0100

    Fix script

--- travis/install-deps.sh
+++ travis/install-deps.sh
@@ -1,18 +1,10 @@
 #! /bin/bash
 
-# enable all trusty repositories
-sudo sh -c "echo 'deb http://archive.ubuntu.com/ubuntu xenial-updates main restricted universe multiverse' >> /etc/apt/sources.list"
-sudo sh -c "echo 'deb http://archive.ubuntu.com/ubuntu xenial-security main restricted universe multiverse' >> /etc/apt/sources.list"
-sudo sh -c "echo 'deb http://archive.ubuntu.com/ubuntu xenial-backports main restricted universe multiverse' >> /etc/apt/sources.list"
-
-cat /etc/apt/sources.list
-
-
 # install proper cross-distro libcurl
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/TheAssassin:/AppImageLibraries/xUbuntu_14.04/ /' > /etc/apt/sources.list.d/curl-httponly.list"
 wget -nv https://download.opensuse.org/repositories/home:TheAssassin:AppImageLibraries/xUbuntu_14.04/Release.key -O- | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install -y curl-httponly libcurl4-gnutls-httponly-dev libcurl3-gnutls-httponly
+sudo apt-get install -y curl-httponly libcurl4-gnutls-httponly-dev libcurl3-gnutls-httponly libcurl3-httponly
 
 # install additional libraries
 sudo apt-get install -y libgit2-dev
