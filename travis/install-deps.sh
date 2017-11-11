5ec13e402a67315b034a234c2fef43ddb0e6e02a
TheAssassin <theassassin@users.noreply.github.com>
2017-11-11 04:26:04 +0100

    Fix script

--- travis/install-deps.sh
+++ travis/install-deps.sh
@@ -4,7 +4,11 @@
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/TheAssassin:/AppImageLibraries/xUbuntu_14.04/ /' > /etc/apt/sources.list.d/curl-httponly.list"
 wget -nv https://download.opensuse.org/repositories/home:TheAssassin:AppImageLibraries/xUbuntu_14.04/Release.key -O- | sudo apt-key add -
 sudo apt-get update
+
+sudo apt-get purge -y curl libcurl\*
+
 sudo apt-get install -y curl-httponly libcurl4-gnutls-httponly-dev libcurl3-gnutls-httponly libcurl3-httponly
+sudo apt-get install -y cmake
 
 # install additional libraries
 sudo apt-get install -y libgit2-dev
