e388e382e9c3f9208e0e797e8b9c6956cd60a40d
TheAssassin <theassassin@users.noreply.github.com>
2017-12-11 03:39:49 +0100

    Don't install unnecessary dependencies

--- travis/bootstrap-container.sh
+++ travis/bootstrap-container.sh
@@ -6,13 +6,3 @@ wget -nv https://download.opensuse.org/repositories/home:TheAssassin:AppImageLib
 sudo apt-get update
 
 sudo apt-get install -y curl libcurl4-gnutls-dev libcurl3-gnutls libcurl3
-
-# update CMake
-wget https://cmake.org/files/v3.10/cmake-3.10.0-rc3-Linux-x86_64.tar.gz -O- | sudo tar xz -C /usr --strip-components=1
-
-# install additional libraries
-sudo apt-get install -y libgit2-dev
-
-# fix setup.py's pygit2 version
-pygit2_version=$(dpkg -l | grep libgit2-dev | awk '{print $3}' | cut -d- -f1)
-sed -i "s|'pygit2'|'pygit2==$pygit2_version'|" setup.py
