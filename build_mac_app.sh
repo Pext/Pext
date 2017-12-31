36dd226e7c3674b8e1451e1ed0d8f6baf3b28eff
Sylvia van Os <sylvia@hackerchick.me>
2017-12-31 03:51:04 +0100

    Add git_describe.py

--- build_mac_app.sh
+++ build_mac_app.sh
@@ -6,6 +6,7 @@ python3 setup.py py2app
 # Copy additional files py2app doesn't copy for some reason
 RESOURCEDIR="dist/Pext.app/Contents/Resources"
 cp "pext/VERSION" "$RESOURCEDIR"
+cp "pext/git_describe.py" "$RESOURCEDIR"
 cp -r "pext/i18n" "$RESOURCEDIR/i18n"
 cp -r "pext/images" "$RESOURCEDIR/images"
 cp -r "pext/qml" "$RESOURCEDIR/qml"
