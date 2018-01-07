c8291f6c7f5d2a4fc47a4ec0b66df1ad5ca99aef
Szöllősi Attila <ata2001@airmail.cc>
2018-01-07 22:42:01 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 100.0% (123 of 123 strings)
    
    Translation: Pext/Translations
    Translate-URL: https://hosted.weblate.org/projects/pext/translations/hu/

--- pext/i18n/pext_hu.ts
+++ pext/i18n/pext_hu.ts
@@ -292,7 +292,7 @@
     <message>
         <location filename="../qml/LoadThemeDialog.qml" line="52"/>
         <source>Note: Pext will restart to apply the new theme.</source>
-        <translation>Megjegyzés: A Pext újraindul, hogy alkalmazza az új témát.</translation>
+        <translation>Megjegyzés: A Pext újraindul az új téma alkalmazása végett.</translation>
     </message>
 </context>
 <context>
@@ -552,7 +552,7 @@
     <message>
         <location filename="../qml/main.qml" line="598"/>
         <source>Sort by module choice</source>
-        <translation>Rendezés modul választás szerint</translation>
+        <translation>Rendezés a modul választása szerint</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="605"/>
