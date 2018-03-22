9a1146801f131f214c99b0adc9afc001c8ad70b9
Sylvia van Os <sylvia@hackerchick.me>
2018-03-22 17:31:06 +0100

    Include pip in macOS py2app setup

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if sys.platform == 'darwin':
             'iconfile': 'pext/images/scalable/pext.icns',
             'emulate_shell_environment': 1,
             'site_packages': 1,
-            'includes': ['readline', 'pty'],
+            'includes': ['readline', 'pty', 'pip'],
             'packages': ['certifi']
         }}
     )
