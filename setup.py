87d237f768d51058362c656bfab49e7eebc62378
Attila Szollosi <ata2001@airmail.cc>
2018-03-28 23:02:06 +0200

    Add pext/VERSION to package_data

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
         'pext/helpers',
         'pext_dev'
     ],
-    package_data={'pext': ['i18n/*.qm', 'images/scalable/*', 'qml/*', 'helpers/*', '*.py'],
+    package_data={'pext': ['VERSION', 'i18n/*.qm', 'images/scalable/*', 'qml/*', 'helpers/*', '*.py'],
                   'pext_dev': ['LICENSE', 'module/*', 'theme/*', '*.py']},
     zip_safe=False,
     entry_points={
