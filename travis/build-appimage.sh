37966c7cb2310124bbfbeb4c1d5a8123b1696646
TheAssassin <theassassin@assassinate-you.net>
2018-03-31 14:27:39 +0200

    Try to fix library errors

--- travis/build-appimage.sh
+++ travis/build-appimage.sh
@@ -53,7 +53,8 @@ wget https://raw.githubusercontent.com/AppImage/AppImages/master/functions.sh
 # back up conda provided libraries -- system one won't work
 mkdir lib-bak
 cp AppDir/usr/lib/*.so* lib-bak/
-(. functions.sh && cd AppDir && set +x && copy_deps && copy_deps && copy_deps && move_lib && delete_blacklisted)
+#(. functions.sh && cd AppDir && set +x && copy_deps && copy_deps && copy_deps && move_lib && delete_blacklisted)
+(. functions.sh && cd AppDir && set +x && move_lib && delete_blacklisted)
 mv AppDir/usr/lib/x86_64-linux-gnu/*.so* AppDir/usr/lib/
 # copy back libraries
 cp --remove-destination lib-bak/* AppDir/usr/lib/
