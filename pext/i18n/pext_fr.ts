2905bfee071a749ddef1a84369ef4c5f6573b5e0
Aurora P <nokasara59@gmail.com>
2018-04-07 16:08:44 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (128 of 128 strings)
    
    Translation: Pext/Translations
    Translate-URL: https://hosted.weblate.org/projects/pext/translations/fr/

--- pext/i18n/pext_fr.ts
+++ pext/i18n/pext_fr.ts
@@ -573,7 +573,7 @@
     <message>
         <location filename="../qml/main.qml" line="625"/>
         <source>Minimizing behaviour</source>
-        <translation type="unfinished">Minimiser le comportement</translation>
+        <translation>Minimiser la page</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="634"/>
@@ -628,7 +628,7 @@
     <message numerus="yes">
         <location filename="../qml/main.qml" line="802"/>
         <source>To get started, press &lt;kbd&gt;%1&lt;/kbd&gt; to open a new tab. When you are done with a tab, you can always close it by pressing &lt;kbd&gt;%2&lt;/kbd&gt;. You currently have %n module(s) installed. You can manage modules in the Module menu.</source>
-        <translation type="unfinished"><numerusform>Pour commencer, appuyez sur &lt;kbd&gt;%1&lt;/ kbd&gt; pour ouvrir un nouvel onglet. Lorsque vous avez terminé avec un onglet, vous pouvez toujours le fermer en appuyant sur &lt;kbd&gt;%2&lt;/ kbd&gt;. Vous avez actuellement %n module(s) installé. Vous pouvez gérer les modules dans le menu Module.</numerusform>
+        <translation><numerusform>Pour commencer, appuyez sur &lt;kbd&gt;%1&lt;/ kbd&gt; pour ouvrir un nouvel onglet. Lorsque vous avez terminé avec un onglet, vous pouvez toujours le fermer en appuyant sur &lt;kbd&gt;%2&lt;/ kbd&gt;. Vous avez actuellement %n module(s) installé. Vous pouvez gérer les modules dans le menu Module.</numerusform>
         <numerusform>Pour commencer, appuyez sur &lt;kbd&gt;%1&lt;/ kbd&gt; pour ouvrir un nouvel onglet. Lorsque vous avez terminé avec un onglet, vous pouvez toujours le fermer en appuyant sur &lt;kbd&gt;%2&lt;/ kbd&gt;. Vous avez actuellement %n module(s) installé. Vous pouvez gérer les modules dans le menu Module.</numerusform>
         </translation></message>
     <message>
