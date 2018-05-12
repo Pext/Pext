47ee18b3a857ff01f22947eb505233d52ce49860
Sylvia van Os <sylvia@hackerchick.me>
2018-05-12 14:33:08 +0200

    Build failing on an .icns file, ugh

--- travis/build-dmg.sh
+++ travis/build-dmg.sh
@@ -67,7 +67,7 @@ popd
 # generate .dmg
 git clone https://github.com/andreyvit/yoursway-create-dmg.git
 pushd yoursway-create-dmg
-bash ./create-dmg --volname "Pext" --volicon "../Pext.app/Contents/Resources/usr/local/pext/images/scalable/pext.icns" --window-pos 200 120 --window-size 800 400 --icon-size 100 --icon Pext.app 200 190 --hide-extension Pext.app --app-drop-link 600 185 Pext.dmg "$BUILD_DIR"/
+bash ./create-dmg --volname "Pext" --volicon "$BUILD_DIR/Pext.app/Contents/Resources/usr/local/pext/images/scalable/pext.icns" --window-pos 200 120 --window-size 800 400 --icon-size 100 --icon Pext.app 200 190 --hide-extension Pext.app --app-drop-link 600 185 Pext.dmg "$BUILD_DIR"/
 
 # move dmg to old CWD
 mv Pext.dmg "$OLD_CWD"/
