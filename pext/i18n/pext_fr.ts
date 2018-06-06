e536742169cc494c487592560e759c60cc72d878
Sylvia van Os <sylvia@hackerchick.me>
2018-06-06 17:03:41 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (125 of 125 strings)
    
    Translation: Pext/Translations
    Translate-URL: https://hosted.weblate.org/projects/pext/translations/fr/

--- pext/i18n/pext_fr.ts
+++ pext/i18n/pext_fr.ts
@@ -114,7 +114,7 @@
     <message>
         <location filename="../qml/InstallFromRepositoryDialog.qml" line="151"/>
         <source>Only install modules from people you trust.</source>
-        <translation type="unfinished"></translation>
+        <translation>Installer uniquement les modules des personnes de confiance.</translation>
     </message>
 </context>
 <context>
@@ -198,7 +198,7 @@
     <message>
         <location filename="../qml/InstallModuleFromURLDialog.qml" line="42"/>
         <source>Only install modules from people you trust.</source>
-        <translation type="unfinished"></translation>
+        <translation>Installer uniquement les modules des personnes de confiance.</translation>
     </message>
 </context>
 <context>
