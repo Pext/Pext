5df45934e7e5acef8f87aacf2d0950528fc53347
Sylvia van Os <sylvia@hackerchick.me>
2018-06-07 07:34:29 +0200

    Switch to own fork of create-dmg to prevent Travis timeouts

--- travis/build-dmg.sh
+++ travis/build-dmg.sh
@@ -66,8 +66,8 @@ chmod a+x Pext.app/Contents/MacOS/Pext
 popd
 
 # generate .dmg
-git clone https://github.com/andreyvit/yoursway-create-dmg.git
-pushd yoursway-create-dmg
+git clone -b patch-2 --single-branch https://github.com/TheLastProject/create-dmg.git
+pushd create-dmg
 bash ./create-dmg --volname "Pext" --volicon "$OLD_CWD"/pext/images/scalable/pext.icns --window-pos 200 120 --window-size 800 400 --icon-size 100 --icon Pext.app 200 190 --hide-extension Pext.app --app-drop-link 600 185 Pext.dmg "$BUILD_DIR"/
 
 # move dmg to old CWD
