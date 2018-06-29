9aec9feb5e9473335c0259c976384bbbbbc0ce2b
Heimen Stoffels <vistausss@outlook.com>
2018-06-29 12:19:15 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (131 of 131 strings)
    
    Translation: Pext/Translations
    Translate-URL: https://hosted.weblate.org/projects/pext/translations/nl/

--- pext/i18n/pext_nl.ts
+++ pext/i18n/pext_nl.ts
@@ -364,7 +364,7 @@
     <message>
         <location filename="../qml/ModuleData.qml" line="159"/>
         <source>No results</source>
-        <translation type="unfinished"></translation>
+        <translation>Geen resultaten</translation>
     </message>
 </context>
 <context>
