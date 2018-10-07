f4e9dd6857bab559b2ea88b238f958e4dbdaf8f2
Sylvia van Os <sylvia@hackerchick.me>
2018-10-07 14:22:40 +0200

    mk 4

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ except Exception as e:
         print("Failed to determine version with git describe: {}".format(e))
 
 if isinstance(version, bytes):
-    version = version.encode()
+    version = version.decode()
 
 if version.startswith('v'):
     version = version[1:]
