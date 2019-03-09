70e8fa2dfc23905cdafa7cde382043820dd1ac56
Sylvia van Os <sylvia@hackerchick.me>
2019-03-09 15:18:32 +0100

    Update translatables

--- pext/i18n/pext_be.ts
+++ pext/i18n/pext_be.ts
@@ -729,6 +729,111 @@ Are you sure you want to quit?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../qml/main.qml" line="972"/>
+        <source>Module&apos;s Module class does not implement ModuleBase</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="973"/>
+        <source>Failed to load module {0}: {1} function has {2} parameters (excluding self), expected {3}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="974"/>
+        <source>{0} is already installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="975"/>
+        <source>Downloading {0} from {1}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="976"/>
+        <source>Failed to download {0}: {1}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="977"/>
+        <source>Downloading dependencies for {0}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="978"/>
+        <source>Failed to download dependencies for {0}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="979"/>
+        <source>Installed {0}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="980"/>
+        <source>Uninstalling {0}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="981"/>
+        <source>{0} is already uninstalled</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="982"/>
+        <source>Uninstalled {0}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="983"/>
+        <source>Updating {0}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="984"/>
+        <source>{0} is already up to date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="985"/>
+        <source>Failed to download update for {0}: {1}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="986"/>
+        <source>Updating dependencies for {0}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="987"/>
+        <source>Failed to update dependencies for {0}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="988"/>
+        <source>Updated {0}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="989"/>
+        <source>Checking for Pext updates</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="990"/>
+        <source>Failed to check for Pext updates: {0}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="991"/>
+        <source>Pext is already up-to-date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="992"/>
+        <source>Data copied to clipboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <location filename="../qml/main.qml" line="438"/>
         <source>Manage modules</source>
         <translation>Упраўленне модулямі</translation>
