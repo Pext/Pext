cfa3c5ffbab1d48a3ec56cd03f45f1c3de48d433
Sylvia van Os <sylvia@hackerchick.me>
2022-06-10 19:55:28 +0200

    Try libcurl

--- ci/bootstrap-container.sh
+++ ci/bootstrap-container.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 sudo apt-get update
-sudo apt-get install -y curl libcurl4-gnutls-dev libcurl3-gnutls libcurl3 bc python3 python3-venv python3-dev qttools5-dev-tools
+sudo apt-get install -y curl libcurl bc python3 python3-venv python3-dev qttools5-dev-tools
