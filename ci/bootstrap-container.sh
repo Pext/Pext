9ca55464127071631ad7cfb31434fe64e88dfc57
Israel Roldan <israel.alberto.rv@gmail.com>
2022-10-21 13:02:59 -0500

    Installed Python 3.6 directly in Ubuntu.

--- ci/bootstrap-container.sh
+++ ci/bootstrap-container.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 
+set -xv
+
 # install Python 3.6
-sudo add-apt-repository -y ppa:deadsnakes/ppa
+#sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get update
-sudo apt-get install -y python3.6 python3.6-venv python3.6-dev
+sudo apt-get install python3.6
+#sudo apt-get install -y python3.6 python3.6-venv python3.6-dev
 
 # install proper cross-distro libcurl
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/TheAssassin:/AppImageLibraries/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/curl-httponly.list"
