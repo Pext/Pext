4d4be53a2c8a541d29f827c8ba868ab9d70b7b23
Israel Roldan <israel.alberto.rv@gmail.com>
2022-10-21 13:15:16 -0500

    Updated the APT key for The Assassin.

--- ci/bootstrap-container.sh
+++ ci/bootstrap-container.sh
@@ -9,8 +9,13 @@ sudo apt-get install python3.6
 #sudo apt-get install -y python3.6 python3.6-venv python3.6-dev
 
 # install proper cross-distro libcurl
+ls -lha .
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/TheAssassin:/AppImageLibraries/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/curl-httponly.list"
-wget https://download.opensuse.org/repositories/home:/TheAssassin:/AppImageLibraries/xUbuntu_18.04/Release.key -O- | sudo apt-key add -
+#wget https://download.opensuse.org/repositories/home:/TheAssassin:/AppImageLibraries/xUbuntu_18.04/Release.key -O- && sudo apt-key add -
+wget -nv https://download.opensuse.org/repositories/home:/TheAssassin:/AppImageLibraries/xUbuntu_18.04/Release.key -O Release.key
+ls -lha .
+#sudo apt-key add - < Release.key
+sudo cat Release.key | sudo apt-key add -
 sudo apt-get update
 
 sudo apt-get install -y curl libcurl4-gnutls-dev libcurl3-gnutls libcurl3 bc
