79d26554a4d2352bf0f1565de6cf2abbe0ead98c
Israel Roldan <israel.alberto.rv@gmail.com>
2022-10-22 01:08:50 -0500

    Fixed the CI packing for Linux.

--- ci/bootstrap-container.sh
+++ ci/bootstrap-container.sh
@@ -1,17 +1,5 @@
 #! /bin/bash
 
-set -xv
-
-sudo uname -a
-sudo lsb_release -a
-sudo cat /proc/version
-sudo arch
-sudo lscpu
-
-# VERSION="18.04.6 LTS (Bionic Beaver)"
-# VERSION_CODENAME=bionic
-# UBUNTU_CODENAME=bionic
-
 # Install Python 3.8
 sudo apt-get update
 sudo apt-get install python3.8 python3.8-venv python3.8-dev
@@ -33,3 +21,10 @@ sudo apt-get install -y curl libcurl4 libcurl4-gnutls-dev bc
 sudo add-apt-repository -y ppa:beineri/opt-qt-5.14.2-bionic
 sudo apt-get update
 sudo apt-get install -y qt514tools
+
+# Create the Python vEnv and install requirements
+/usr/bin/python3.8 -m venv pext-env
+source pext-env/bin/activate
+pip install --upgrade pip
+pip install tox-travis
+pip install -r requirements.txt
