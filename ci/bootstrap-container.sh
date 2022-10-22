276521b97ed55378ca2a37fe6911ab7a40ea8e14
Israel Roldan <israel.alberto.rv@gmail.com>
2022-10-21 20:13:47 -0500

    Updated http to https for the assassin repository.

--- ci/bootstrap-container.sh
+++ ci/bootstrap-container.sh
@@ -9,14 +9,23 @@ sudo apt-get install python3.6
 #sudo apt-get install -y python3.6 python3.6-venv python3.6-dev
 
 # install proper cross-distro libcurl
-ls -lha .
-sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/TheAssassin:/AppImageLibraries/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/curl-httponly.list"
+sudo sh -c "echo 'deb https://download.opensuse.org/repositories/home:/TheAssassin:/AppImageLibraries/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/curl-httponly.list"
+echo "###########################################"
 #wget https://download.opensuse.org/repositories/home:/TheAssassin:/AppImageLibraries/xUbuntu_18.04/Release.key -O- && sudo apt-key add -
+echo "###########################################"
 wget -nv https://download.opensuse.org/repositories/home:/TheAssassin:/AppImageLibraries/xUbuntu_18.04/Release.key -O Release.key
-ls -lha .
+echo "###########################################"
+ls -lhaR /etc/apt/sources.list.d/
+echo "###########################################"
+cat /etc/apt/sources.list.d/curl-httponly.list
 #sudo apt-key add - < Release.key
+echo "###########################################"
 sudo apt-key add Release.key
+echo "###########################################"
+sudo rm -f Release.key
+echo "###########################################"
 sudo apt-get update
+echo "###########################################"
 
 sudo apt-get install -y curl libcurl4-gnutls-dev libcurl3-gnutls libcurl3 bc
 
