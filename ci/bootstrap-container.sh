a12bb80299719705cae163ad50bd0f5b30a88a3a
Israel Roldan <israel.alberto.rv@gmail.com>
2022-10-23 21:41:21 -0500

    Fixed the debug information to the Linux action.

--- ci/bootstrap-container.sh
+++ ci/bootstrap-container.sh
@@ -1,20 +1,6 @@
 #! /bin/bash
 
-echo "##################################################"
-sudo echo "$VERSION" || true
-echo "##################################################"
-sudo cat /etc/os-release | grep UBUNTU_CODENAME | cut -d = -f 2
-echo "##################################################"
-sudo lsb_release -cs
-echo "##################################################"
-sudo lsb_release -a || true
-echo "##################################################"
-sudo cat /etc/os-release || true
-echo "##################################################"
-sudo cat /etc/issue || true
-echo "##################################################"
-sudo hostnamectl || true
-echo "##################################################"
+UBUNTU_CODENAME=$(sudo lsb_release -cs)
 
 # Install Python
 sudo apt-get update
@@ -23,7 +9,18 @@ sudo apt search venv
 echo "###################################################"
 sudo apt search python3.
 echo "###################################################"
-sudo apt-get install -y python3.8 python3.8-venv python3.8-dev
+echo "UBUNTU_CODENAME: ${UBUNTU_CODENAME}"
+if [ "${UBUNTU_CODENAME}" -eq jammy ]; then
+  sudo apt-get install -y python3.10 python3.10-venv python3.10-dev
+elif [ "${UBUNTU_CODENAME}" -eq focal ]; then
+  sudo apt-get install -y python3.9 python3.9-venv python3.9-dev
+elif [ "${UBUNTU_CODENAME}" -eq bionic ]; then
+  sudo apt-get install -y python3.8 python3.8-venv python3.8-dev
+else
+  echo "ERROR: The Ubuntu version '${UBUNTU_CODENAME}' is outside the scope (bionic, focal or jammy)."
+  sudo lsb_release -a
+  exit 1
+fi
 
 # Install curl
 sudo apt-get install -y curl libcurl4 libcurl4-gnutls-dev bc
@@ -34,7 +31,13 @@ sudo apt-get update
 sudo apt-get install -y qt514tools
 
 # Create the Python vEnv and install requirements
-/usr/bin/python3.8 -m venv pext-env
+if [ "${UBUNTU_CODENAME}" -eq jammy ]; then
+  /usr/bin/python3.10 -m venv pext-env
+elif [ "${UBUNTU_CODENAME}" -eq focal ]; then
+  /usr/bin/python3.9 -m venv pext-env
+elif [ "${UBUNTU_CODENAME}" -eq bionic ]; then
+  /usr/bin/python3.8 -m venv pext-env
+fi
 source pext-env/bin/activate
 pip install --upgrade pip
 pip install tox-travis
