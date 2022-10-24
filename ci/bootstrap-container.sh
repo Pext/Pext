ef1f177949e2ca1d02c4dc47e7ebfa93dcae6b01
Israel Roldan <israel.alberto.rv@gmail.com>
2022-10-23 23:10:47 -0500

    Fixed the debug information to the Linux action.

--- ci/bootstrap-container.sh
+++ ci/bootstrap-container.sh
@@ -1,20 +1,10 @@
 #! /bin/bash
 
 UBUNTU_CODENAME=$(sudo lsb_release -cs)
-echo "UBUNTU_CODENAME: ${UBUNTU_CODENAME}"
+UBUNTU_RELEASE=$(sudo lsb_release -rs)
 
 # Install Python
 sudo apt-get update
-echo "######################################"
-sudo lsb_release -a
-echo "######################################"
-sudo lsb_release -i
-echo "######################################"
-sudo lsb_release -d
-echo "######################################"
-sudo lsb_release -r
-echo "######################################"
-sudo lsb_release --help
 echo "######################################"
 sudo python --version
 echo "######################################"
@@ -37,14 +27,14 @@ sudo apt-get install -y curl libcurl4 libcurl4-gnutls-dev bc
 
 # Install Qt
 if [ "${UBUNTU_CODENAME}" = "jammy" ]; then
-  sudo add-apt-repository -y ppa:beineri/opt-qt-5.14.2-focal
+  sudo add-apt-repository -y ppa:beineri/opt-qt-5.14.2-jammy
 elif [ "${UBUNTU_CODENAME}" = "focal" ]; then
-  sudo add-apt-repository -y ppa:beineri/opt-qt-5.15.4-focal
+  sudo add-apt-repository -y ppa:beineri/opt-qt-5.14.2-focal
 elif [ "${UBUNTU_CODENAME}" = "bionic" ]; then
-  sudo add-apt-repository -y ppa:beineri/opt-qt-5.15.2-bionic
+  sudo add-apt-repository -y ppa:beineri/opt-qt-5.14.2-bionic
 fi
 sudo apt-get update
-sudo apt-get install -y qt515tools
+sudo apt-get install -y qt514tools
 
 # Create the Python vEnv and install requirements
 if [ "${UBUNTU_CODENAME}" = "jammy" ]; then
@@ -59,13 +49,22 @@ pip install --upgrade pip
 pip install tox-travis
 pip install -r requirements.txt
 
-source /opt/qt515/bin/qt515-env.sh || true
+# Generate translation
+source /opt/qt514/bin/qt514-env.sh || true
 bash -xe prepare_activate_translations.sh 70 "https://hosted.weblate.org/exports/stats/pext/?format=json"
 lrelease pext/pext.pro
 
+# Run tests
 source pext-env/bin/activate
-xvfb-run tox -v -e py38
+if [ "${UBUNTU_CODENAME}" = "jammy" ]; then
+  xvfb-run tox -v -e py310
+elif [ "${UBUNTU_CODENAME}" = "focal" ]; then
+  xvfb-run tox -v -e py39
+elif [ "${UBUNTU_CODENAME}" = "bionic" ]; then
+  xvfb-run tox -v -e py38
+fi
 
+# Build the app
 mkdir -p build
 cd build && exit 1
 pwd
