36c9b0aad91b4657c71647a8bfb407ec6341c4b5
Israel Roldan <israel.alberto.rv@gmail.com>
2022-10-24 01:08:51 -0500

    Fixed the debug information to the Linux action.

--- ci/bootstrap-container.sh
+++ ci/bootstrap-container.sh
@@ -28,7 +28,8 @@ fi
 sudo apt-get install -y curl libcurl4 libcurl4-gnutls-dev bc
 
 # Install Qt
-sudo apt-get install -y qt5-default qt5-qmake qtbase5-dev-tools qttools5-dev-tools
+#sudo apt-get install -y qt5-default qt5-qmake qtbase5-dev-tools qttools5-dev-tools
+sudo apt-get install -y qtbase5-dev qt5-qmake qtbase5-dev-tools qttools5-dev-tools
 
 # Create the Python vEnv and install requirements
 if [ "${UBUNTU_CODENAME}" = "jammy" ]; then
