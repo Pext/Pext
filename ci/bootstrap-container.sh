cb1bd53c576349e6b2105aef5299c89049bfbe75
Israel Roldan <israel.alberto.rv@gmail.com>
2022-10-24 02:00:51 -0500

    Fixed the debug information to the Linux action.

--- ci/bootstrap-container.sh
+++ ci/bootstrap-container.sh
@@ -1,20 +1,19 @@
 #! /bin/bash
 
-UBUNTU_CODENAME=$(sudo lsb_release -cs)
 UBUNTU_RELEASE=$(sudo lsb_release -rs)
 
 # Update APT repositories
 sudo apt-get update
 
 # Install Python
-if [ "${UBUNTU_CODENAME}" = "jammy" ]; then
+if [ "${UBUNTU_RELEASE}" = "ubuntu-22.04" ]; then
   sudo apt-get install -y python3.10 python3.10-venv python3.10-dev
-elif [ "${UBUNTU_CODENAME}" = "focal" ]; then
+elif [ "${UBUNTU_RELEASE}" = "ubuntu-20.04" ]; then
   sudo apt-get install -y python3.9 python3.9-venv python3.9-dev
-elif [ "${UBUNTU_CODENAME}" = "bionic" ]; then
+elif [ "${UBUNTU_RELEASE}" = "ubuntu-18.04" ]; then
   sudo apt-get install -y python3.8 python3.8-venv python3.8-dev
 else
-  echo "ERROR: The Ubuntu version '${UBUNTU_CODENAME}' is outside the scope (bionic, focal or jammy)."
+  echo "ERROR: The Ubuntu version '${UBUNTU_RELEASE}' is outside the scope (ubuntu-18.04, ubuntu-20.04 or ubuntu-22.04)."
   lsb_release -a
   exit 1
 fi
@@ -23,18 +22,18 @@ fi
 sudo apt-get install -y curl libcurl4 libcurl4-gnutls-dev bc
 
 # Install Qt
-if [ "${UBUNTU_CODENAME}" = "jammy" ]; then
+if [ "${UBUNTU_RELEASE}" = "ubuntu-22.04" ]; then
   sudo apt-get install -y qtbase5-dev qt5-qmake qtbase5-dev-tools qttools5-dev-tools
 else
   sudo apt-get install -y qt5-default qt5-qmake qtbase5-dev-tools qttools5-dev-tools
 fi
 
 # Create the Python vEnv and install requirements
-if [ "${UBUNTU_CODENAME}" = "jammy" ]; then
+if [ "${UBUNTU_RELEASE}" = "ubuntu-22.04" ]; then
   /usr/bin/python3.10 -m venv pext-env
-elif [ "${UBUNTU_CODENAME}" = "focal" ]; then
+elif [ "${UBUNTU_RELEASE}" = "ubuntu-20.04" ]; then
   /usr/bin/python3.9 -m venv pext-env
-elif [ "${UBUNTU_CODENAME}" = "bionic" ]; then
+elif [ "${UBUNTU_RELEASE}" = "ubuntu-18.04" ]; then
   /usr/bin/python3.8 -m venv pext-env
 fi
 source pext-env/bin/activate
@@ -48,11 +47,11 @@ lrelease pext/pext.pro
 
 # Run tests
 source pext-env/bin/activate
-if [ "${UBUNTU_CODENAME}" = "jammy" ]; then
+if [ "${UBUNTU_RELEASE}" = "ubuntu-22.04" ]; then
   xvfb-run tox -v -e py310
-elif [ "${UBUNTU_CODENAME}" = "focal" ]; then
+elif [ "${UBUNTU_RELEASE}" = "ubuntu-20.04" ]; then
   xvfb-run tox -v -e py39
-elif [ "${UBUNTU_CODENAME}" = "bionic" ]; then
+elif [ "${UBUNTU_RELEASE}" = "ubuntu-18.04" ]; then
   xvfb-run tox -v -e py38
 fi
 
