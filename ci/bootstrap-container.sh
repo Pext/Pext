dad41b72865774af5262ff3f7783fc81bf426460
Israel Roldan <israel.alberto.rv@gmail.com>
2022-10-23 22:58:26 -0500

    Fixed the debug information to the Linux action.

--- ci/bootstrap-container.sh
+++ ci/bootstrap-container.sh
@@ -6,6 +6,16 @@ echo "UBUNTU_CODENAME: ${UBUNTU_CODENAME}"
 # Install Python
 sudo apt-get update
 echo "######################################"
+sudo lsb_release -a
+echo "######################################"
+sudo lsb_release -i
+echo "######################################"
+sudo lsb_release -d
+echo "######################################"
+sudo lsb_release -r
+echo "######################################"
+sudo lsb_release --help
+echo "######################################"
 sudo python --version
 echo "######################################"
 python --version
@@ -26,16 +36,22 @@ fi
 sudo apt-get install -y curl libcurl4 libcurl4-gnutls-dev bc
 
 # Install Qt
-sudo add-apt-repository -y ppa:beineri/opt-qt-5.14.2-"${UBUNTU_CODENAME}"
+if [ "${UBUNTU_CODENAME}" = "jammy" ]; then
+  sudo add-apt-repository -y ppa:beineri/opt-qt-5.14.2-focal
+elif [ "${UBUNTU_CODENAME}" = "focal" ]; then
+  sudo add-apt-repository -y ppa:beineri/opt-qt-5.15.4-focal
+elif [ "${UBUNTU_CODENAME}" = "bionic" ]; then
+  sudo add-apt-repository -y ppa:beineri/opt-qt-5.15.2-bionic
+fi
 sudo apt-get update
-sudo apt-get install -y qt514tools
+sudo apt-get install -y qt515tools
 
 # Create the Python vEnv and install requirements
-if [ "${UBUNTU_CODENAME}" -eq jammy ]; then
+if [ "${UBUNTU_CODENAME}" = "jammy" ]; then
   /usr/bin/python3.10 -m venv pext-env
-elif [ "${UBUNTU_CODENAME}" -eq focal ]; then
+elif [ "${UBUNTU_CODENAME}" = "focal" ]; then
   /usr/bin/python3.9 -m venv pext-env
-elif [ "${UBUNTU_CODENAME}" -eq bionic ]; then
+elif [ "${UBUNTU_CODENAME}" = "bionic" ]; then
   /usr/bin/python3.8 -m venv pext-env
 fi
 source pext-env/bin/activate
@@ -43,8 +59,17 @@ pip install --upgrade pip
 pip install tox-travis
 pip install -r requirements.txt
 
-echo "###################################################"
-sudo apt search venv
-echo "###################################################"
-sudo apt search python3.
-echo "###################################################"
+source /opt/qt515/bin/qt515-env.sh || true
+bash -xe prepare_activate_translations.sh 70 "https://hosted.weblate.org/exports/stats/pext/?format=json"
+lrelease pext/pext.pro
+
+source pext-env/bin/activate
+xvfb-run tox -v -e py38
+
+mkdir -p build
+cd build && exit 1
+pwd
+ls -lha
+pwd
+ls -lha ../
+bash -xe ../ci/build-appimage.sh
